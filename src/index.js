238735ae19c7d1c4e5f073ebbeb9cb09ab7b5984
tuber84 <97830928+tuber84@users.noreply.github.com>
2022-02-12 14:38:08 +0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+let counter = 0;
+//counter++;
+//let count = counter;
+
+let value = +true;
+let str = +"5";
+let num = typeof Number("123");
+//let v = +prompt("первое число","");
+
+console.log(counter);
+console.log(value);
+console.log(`${typeof v}`);
+
+let a = "2";
+let b = "3";
+let c = +a + +b;
+console.log(c);
